a929d17d8a682f3ea6a06950f2fcf2b54dde8685
G4br13l3 <gabriele.ponzi@gmail.com>
2016-06-17 12:50:24 +0200

    Building fixed

---
+++ teleporter/src/test/java/com/orientechnologies/teleporter/test/rdbms/main/TeleporterBasicExecutionTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 OrientDB LTD (info--at--orientdb.com)
+ * All Rights Reserved. Commercial License.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * OrientDB LTD and its suppliers, if any.  The intellectual and
+ * technical concepts contained herein are proprietary to
+ * OrientDB LTD and its suppliers and may be covered by United
+ * Kingdom and Foreign Patents, patents in process, and are protected by trade
+ * secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from OrientDB LTD.
+ *
+ * For more information: http://www.orientdb.com
+ */
+
+package com.orientechnologies.teleporter.test.rdbms.main;
+
+import com.orientechnologies.teleporter.main.OTeleporter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author Gabriele Ponzi
+ * @email <gabriele.ponzi--at--gmail.com>
+ *
+ */
+
+public class TeleporterBasicExecutionTest extends TeleporterInvocationTest {
+
+  @Before
+  public void init() {
+    buildEnvironmentForExecution();
+    prepareArguments();
+    prepareArrayArgs();
+  }
+
+  @Test
+  public void test1() {
+
+    boolean jobComplete = false;
+    int retry = 0;
+
+    while(!jobComplete) {
+
+      try {
+        OTeleporter.main(args);
+        jobComplete = true;
+      } catch (Exception e) {
+        e.printStackTrace();
+        if(retry < 3) {
+          System.out.printf("Job failed, restarting job (retry " + (retry+1) + "/3)");
+          retry++;
+          continue;
+        }
+        else {
+          fail("Job failed " + (retry + 1) + " times!\n" + e.getMessage());
+        }
+      }
+    }
+
+  }
+
+
+}
---
+++ teleporter/src/test/java/com/orientechnologies/teleporter/test/rdbms/main/TeleporterInvocationTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2015 OrientDB LTD (info--at--orientdb.com)
+ * All Rights Reserved. Commercial License.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * OrientDB LTD and its suppliers, if any.  The intellectual and
+ * technical concepts contained herein are proprietary to
+ * OrientDB LTD and its suppliers and may be covered by United
+ * Kingdom and Foreign Patents, patents in process, and are protected by trade
+ * secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from OrientDB LTD.
+ *
+ * For more information: http://www.orientdb.com
+ */
+
+package com.orientechnologies.teleporter.test.rdbms.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.fail;
+
+/**
+ * @author Gabriele Ponzi
+ * @email  <gabriele.ponzi--at--gmail.com>
+ *
+ */
+
+public abstract class TeleporterInvocationTest {
+
+  // arguments
+  protected Map<String,String> arguments = new HashMap<String,String>();
+
+  protected String[] args;
+
+  protected Connection dbConnection;
+
+
+  protected void buildEnvironmentForExecution() {
+      this.buildHSQLDBDatabaseToImport();
+  }
+
+  private void buildHSQLDBDatabaseToImport() {
+    try {
+      Class.forName("org.hsqldb.jdbc.JDBCDriver");
+      this.dbConnection = DriverManager.getConnection("jdbc:hsqldb:mem:mydb", "SA", "");
+
+      // Tables Building
+
+      String directorTableBuilding = "create memory table DIRECTOR (ID varchar(256) not null, NAME  varchar(256),"+
+          " SURNAME varchar(256) not null, primary key (ID))";
+      Statement st = this.dbConnection.createStatement();
+      st.execute(directorTableBuilding);
+
+      String categoryTableBuilding = "create memory table CATEGORY (ID varchar(256) not null, NAME  varchar(256), primary key (ID))";
+      st.execute(categoryTableBuilding);
+
+      String filmTableBuilding = "create memory table FILM (ID varchar(256) not null,"+
+          " TITLE varchar(256) not null, DIRECTOR varchar(256) not null, CATEGORY varchar(256) not null," +
+          " primary key (ID), " +
+          " foreign key (DIRECTOR) references DIRECTOR(ID)," +
+          " foreign key (CATEGORY) references CATEGORY(ID))";
+      st.execute(filmTableBuilding);
+
+      String actorTableBuilding = "create memory table ACTOR (ID varchar(256) not null, NAME  varchar(256),"+
+          " SURNAME varchar(256) not null, primary key (ID))";
+      st.execute(actorTableBuilding);
+
+      String film2actorTableBuilding = "create memory table FILM_ACTOR (FILM_ID varchar(256) not null, ACTOR_ID  varchar(256),"+
+          " primary key (FILM_ID,ACTOR_ID), foreign key (FILM_ID) references FILM(ID), foreign key (ACTOR_ID) references ACTOR(ID))";
+      st.execute(film2actorTableBuilding);
+
+
+      // Records Inserting
+
+      String directorFilling = "insert into DIRECTOR (ID,NAME,SURNAME) values ("
+          + "('D001','Quentin','Tarantino'),"
+          + "('D002','Martin','Scorsese'))";
+      st.execute(directorFilling);
+
+      String categoryFilling = "insert into CATEGORY (ID,NAME) values ("
+          + "('C001','Thriller'),"
+          + "('C002','Action'),"
+          + "('C003','Sci-Fi'),"
+          + "('C004','Fantasy'),"
+          + "('C005','Comedy'),"
+          + "('C006','Drama'),"
+          + "('C007','War'))";
+      st.execute(categoryFilling);
+
+      String filmFilling = "insert into FILM (ID,TITLE,DIRECTOR,CATEGORY) values ("
+          + "('F001','Pulp Fiction','D001','C002'),"
+          + "('F002','Shutter Island','D002','C001'),"
+          + "('F003','The Departed','D002','C001'))";
+      st.execute(filmFilling);
+
+      String actorFilling = "insert into ACTOR (ID,NAME,SURNAME) values ("
+          + "('A001','John','Travolta'),"
+          + "('A002','Samuel','Lee Jackson'),"
+          + "('A003','Bruce','Willis'),"
+          + "('A004','Leonardo','Di Caprio'),"
+          + "('A005','Ben','Kingsley'),"
+          + "('A006','Mark','Ruffalo'),"
+          + "('A007','Jack','Nicholson'),"
+          + "('A008','Matt','Damon'))";
+      st.execute(actorFilling);
+
+      String film2actorFilling = "insert into FILM_ACTOR (FILM_ID,ACTOR_ID) values ("
+          + "('F001','A001'),"
+          + "('F001','A002'),"
+          + "('F001','A003'),"
+          + "('F002','A004'),"
+          + "('F002','A005'),"
+          + "('F002','A006'),"
+          + "('F003','A004'),"
+          + "('F003','A007'),"
+          + "('F003','A008'))";
+      st.execute(film2actorFilling);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail(e.getMessage());
+    }
+  }
+
+    public void prepareArguments() {
+      this.arguments.put("-jdriver", "hypersql");
+      this.arguments.put("-jurl", "jdbc:hsqldb:mem:mydb");
+      this.arguments.put("-ourl", "memory:testOrientDB");
+      this.arguments.put("-juser", "SA");
+      this.arguments.put("-jpasswd", "");
+    }
+
+  public void prepareArrayArgs() {
+
+    args = new String[arguments.size()*2];
+
+    int i = 0;
+    for(String key: arguments.keySet()) {
+      args[i] = key;
+      args[i+1] = arguments.get(key);
+      i = i+2;
+    }
+
+  }
+
+}
