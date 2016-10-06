b0a3268a419793f7df36ebe632394fc33a44a537
G4br13l3 <gabriele.ponzi@gmail.com>
2016-10-06 20:08:15 +0100

    ODocumentComaparator implemented

--- teleporter/src/main/java/com/orientechnologies/teleporter/util/ODocumentComparator.java
+++ teleporter/src/main/java/com/orientechnologies/teleporter/util/ODocumentComparator.java
@@ -10,6 +10,7 @@ import java.util.List;
  * Created by gabriele on 05/10/16.
  */
 public class ODocumentComparator {
+    private static int invocation = 0;
 
     public static boolean areEquals(ODocument doc1, ODocument doc2) {
 
@@ -32,22 +33,22 @@ public class ODocumentComparator {
             if(doc1CurrValue instanceof List && doc2CurrValue instanceof List ) {
                 List<ODocument> doc1CurrValues = doc1.field(fieldName);
                 List<ODocument> doc2CurrValues = doc2.field(fieldName);
-                areListEquals(doc1CurrValues, doc2CurrValues);
-            }
-
-            if( !(doc1CurrValue instanceof ODocument) && (doc2CurrValue instanceof ODocument) ||
-                    (doc1CurrValue instanceof ODocument) && !(doc2CurrValue instanceof ODocument)   ) {
-                return false;
-            }
-
-            if(!(doc1CurrValue instanceof ODocument) && !(doc2CurrValue instanceof ODocument)) {
-                if(!doc1CurrValue.equals(doc1CurrValue)) {
+                if(!areListEquals(doc1CurrValues, doc2CurrValues)) {
                     return false;
                 }
             }
 
-            if(doc1CurrValue instanceof ODocument && doc2CurrValue instanceof ODocument) {
-                if(areEquals(doc1, doc2)) {
+            else if( !(doc1CurrValue instanceof ODocument) && (doc2CurrValue instanceof ODocument) ||
+                    (doc1CurrValue instanceof ODocument) && !(doc2CurrValue instanceof ODocument)   ) {
+                return false;
+            }
+            else if(!(doc1CurrValue instanceof ODocument) && !(doc2CurrValue instanceof ODocument)) {
+                if(!doc1CurrValue.equals(doc2CurrValue)) {  //???? primitives ????
+                    return false;
+                }
+            }
+            else if(doc1CurrValue instanceof ODocument && doc2CurrValue instanceof ODocument) {
+                if(!areEquals((ODocument) doc1CurrValue, (ODocument) doc2CurrValue)) {
                     return false;
                 }
             }
@@ -55,7 +56,39 @@ public class ODocumentComparator {
         return true;
     }
 
-    private static void areListEquals(List<ODocument> doc1CurrValues, List<ODocument> doc2CurrValues) {
+    private static boolean areListEquals(List<ODocument> doc1CurrValues, List<ODocument> doc2CurrValues) {
 
+        if(doc1CurrValues.size() != doc2CurrValues.size()) {
+            return false;
+        }
+
+        for(Object currentDocFromList1: doc1CurrValues) {
+            boolean docPresentInSecondList = false;
+
+            for(Object currentDocFromList2: doc2CurrValues) {
+                if( !(currentDocFromList1 instanceof ODocument) && (currentDocFromList2 instanceof ODocument) ||
+                        (currentDocFromList1 instanceof ODocument) && !(currentDocFromList2 instanceof ODocument)   ) {
+                    continue;
+                }
+                if(!(currentDocFromList1 instanceof ODocument) && !(currentDocFromList2 instanceof ODocument)) {
+                    if(currentDocFromList1.equals(currentDocFromList2)) {  //???? primitives ????
+                        docPresentInSecondList =  true;
+                        break;
+                    }
+                }
+                if(currentDocFromList1 instanceof ODocument && currentDocFromList2 instanceof ODocument) {
+                    if(areEquals((ODocument) currentDocFromList1, (ODocument) currentDocFromList2)) {
+                        docPresentInSecondList =  true;
+                        break;
+                    }
+                }
+            }
+
+            if(!docPresentInSecondList) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
