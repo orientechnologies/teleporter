b325ee25b818b76c85a7a9a92b063a6cf7726824
G4br13l3 <gabriele.ponzi@gmail.com>
2016-10-07 22:08:40 +0100

    New graph model building strategies (execution to implement)

---
+++ teleporter/src/main/java/com/orientechnologies/teleporter/strategy/rdbms/ODBMSModelBuildingStrategy.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 OrientDB LTD (info--at--orientdb.com)
+ * All Rights Reserved. Commercial License.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * OrientDB LTD and its suppliers, if any.  The intellectual and
+ * technical concepts contained herein are proprietary to
+ * OrientDB LTD and its suppliers and may be covered by United
+ * Kingdom and Foreign Patents, patents in process, and are protected by trade
+ * secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from OrientDB LTD.
+ *
+ * For more information: http://www.orientdb.com
+ */
+
+package com.orientechnologies.teleporter.strategy.rdbms;
+
+import com.orientechnologies.teleporter.context.OTeleporterContext;
+import com.orientechnologies.teleporter.mapper.rdbms.OER2GraphMapper;
+import com.orientechnologies.teleporter.model.OSourceInfo;
+import com.orientechnologies.teleporter.strategy.OWorkflowStrategy;
+
+import java.util.List;
+
+/**
+ * @author Gabriele Ponzi
+ * @email  <gabriele.ponzi--at--gmail.com>
+ *
+ */
+
+public class ODBMSModelBuildingStrategy implements OWorkflowStrategy {
+
+    protected OER2GraphMapper mapper;
+
+    public ODBMSModelBuildingStrategy() {}
+
+    @Override
+    public void executeStrategy(OSourceInfo sourceInfo, String outOrientGraphUri, String chosenMapper, String xmlPath, String nameResolverConvention, List<String> includedTables, List<String> excludedTables, String configurationPath, OTeleporterContext context) {
+
+        //TODO
+
+    }
+}
