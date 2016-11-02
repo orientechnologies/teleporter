e9e37e3f5ec3bf3a762ccd004b508e68e9ea61cf
lvca <l.garulli@gmail.com>
2016-11-02 17:32:08 +0100

    Added new method to parse classes/properties

--- teleporter/src/main/java/com/orientechnologies/teleporter/configuration/api/OEdgeMappingInformation.java
+++ teleporter/src/main/java/com/orientechnologies/teleporter/configuration/api/OEdgeMappingInformation.java
@@ -21,80 +21,98 @@ package com.orientechnologies.teleporter.configuration.api;
 import java.util.List;
 
 /**
- *
  * @author Gabriele Ponzi
  * @email <g.ponzi--at--orientdb.com>
- *
  */
 
 public class OEdgeMappingInformation {
 
-    private OConfiguredEdgeClass belongingEdge;                        // mandatory
-    private String fromTableName;                                      // mandatory
-    private String toTableName;                                        // mandatory
-    private List<String> fromColumns;                                  // mandatory
-    private List<String> toColumns;                                    // mandatory
-    private String direction;                                          // mandatory
-    private OAggregatedJoinTableMapping representedJoinTableMapping;   // may be null if the edge does not represent a join table
+  private OConfiguredEdgeClass        belongingEdge;                        // mandatory
+  private String                      fromTableName;                                      // mandatory
+  private String                      toTableName;                                        // mandatory
+  private List<String>                fromColumns;                                  // mandatory
+  private List<String>                toColumns;                                    // mandatory
+  private String                      direction;                                          // mandatory
+  private OAggregatedJoinTableMapping representedJoinTableMapping;   // may be null if the edge does not represent a join table
 
-    public OEdgeMappingInformation(OConfiguredEdgeClass belongingEdge) {
-        this.belongingEdge = belongingEdge;
-    }
+  public OEdgeMappingInformation(OConfiguredEdgeClass belongingEdge) {
+    this.belongingEdge = belongingEdge;
+  }
 
-    public OConfiguredEdgeClass getBelongingEdge() {
-        return this.belongingEdge;
-    }
+  public OConfiguredEdgeClass getBelongingEdge() {
+    return this.belongingEdge;
+  }
 
-    public void setBelongingEdge(OConfiguredEdgeClass belongingEdge) {
-        this.belongingEdge = belongingEdge;
-    }
+  public void setBelongingEdge(OConfiguredEdgeClass belongingEdge) {
+    this.belongingEdge = belongingEdge;
+  }
 
-    public String getFromTableName() {
-        return this.fromTableName;
-    }
+  public String getFromTableName() {
+    return this.fromTableName;
+  }
 
-    public void setFromTableName(String fromTableName) {
-        this.fromTableName = fromTableName;
-    }
+  public void setFromTableName(String fromTableName) {
+    this.fromTableName = fromTableName;
+  }
 
-    public String getToTableName() {
-        return this.toTableName;
-    }
+  public String getToTableName() {
+    return this.toTableName;
+  }
 
-    public void setToTableName(String toTableName) {
-        this.toTableName = toTableName;
-    }
+  public void setToTableName(String toTableName) {
+    this.toTableName = toTableName;
+  }
 
-    public List<String> getFromColumns() {
-        return this.fromColumns;
-    }
+  public List<String> getFromColumns() {
+    return this.fromColumns;
+  }
 
-    public void setFromColumns(List<String> fromColumns) {
-        this.fromColumns = fromColumns;
-    }
+  public void setFromColumns(List<String> fromColumns) {
+    this.fromColumns = fromColumns;
+  }
 
-    public List<String> getToColumns() {
-        return this.toColumns;
-    }
+  public List<String> getToColumns() {
+    return this.toColumns;
+  }
 
-    public void setToColumns(List<String> toColumns) {
-        this.toColumns = toColumns;
-    }
+  public void setToColumns(List<String> toColumns) {
+    this.toColumns = toColumns;
+  }
 
-    public String getDirection() {
-        return this.direction;
-    }
+  public String getFromClass() {
+    // TODO
+    return null;
+  }
 
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
+  public String getToClass() {
+    // TODO
+    return null;
+  }
 
-    public OAggregatedJoinTableMapping getRepresentedJoinTableMapping() {
-        return this.representedJoinTableMapping;
-    }
+  public String getFromProperty() {
+    // TODO
+    return null;
+  }
 
-    public void setRepresentedJoinTableMapping(OAggregatedJoinTableMapping representedJoinTableMapping) {
-        this.representedJoinTableMapping = representedJoinTableMapping;
-    }
+  public String getToProperty() {
+    // TODO
+    return null;
+  }
+
+  public String getDirection() {
+    return this.direction;
+  }
+
+  public void setDirection(String direction) {
+    this.direction = direction;
+  }
+
+  public OAggregatedJoinTableMapping getRepresentedJoinTableMapping() {
+    return this.representedJoinTableMapping;
+  }
+
+  public void setRepresentedJoinTableMapping(OAggregatedJoinTableMapping representedJoinTableMapping) {
+    this.representedJoinTableMapping = representedJoinTableMapping;
+  }
 
 }
